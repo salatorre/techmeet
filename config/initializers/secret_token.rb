543c1de8a65071ef2afd59c5bbee012391f4e05a
Steven Alatorre <steve.alatorre@gmail.com>
2012-02-04 11:56:19 -0800

    Generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techmeet::Application.config.secret_token = '5ca01560aac05752cb8f0581d8d04ab5b96d5691c66921c7e6dfc55f093a5b33c1e27663594684973002a395bb773d4066aaf9fbf0610b10c95235fab565bbef'
